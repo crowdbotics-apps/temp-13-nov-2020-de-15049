d8eb0b7556d391882abcdf1b6e911341133325c4
Crowdbotics <team@crowdbotics.com>
2020-11-13 06:18:16 +0000

    add component 2020 13 nov

---
+++ src/features/202013Nov214491/screens/blank.js
@@ -0,0 +1,62 @@
+import React from "react"
+import {
+  View,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  Text,
+  Button,
+  Switch,
+  TextInput,
+  StyleSheet
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
+
+export default class Blank extends React.Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      headerLeft: <SlideMenuIcon navigationProps={navigation} />
+    }
+  }
+
+  state = { TextInput_3: "" }
+
+  render = () => (
+    <View>
+      <Text>Sample text content</Text>
+      <TextInput
+        placeholder="Sample text input placeholder"
+        value={this.state.TextInput_3}
+        onChangeText={nextValue => this.setState({ TextInput_3: nextValue })}
+      />
+      <Button title="Press me!" onPress={() => alert("Pressed!")} />
+      <Button title="Press me!" onPress={() => alert("Pressed!")} />
+      <Text>Sample text content</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginHorizontal: 16
+  },
+
+  View_1: {},
+  Text_2: {},
+  TextInput_3: {},
+  Button_4: {},
+  Button_5: {},
+  Text_6: {},
+  Switch_7: { alignSelf: "flex-start" },
+  View_1: {},
+  Text_2: {},
+  TextInput_3: {},
+  Button_4: {},
+  Button_5: {},
+  Text_6: {}
+})
